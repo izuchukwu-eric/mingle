866b73430f0224b207623a40fa324bf8df5a57d3
izuchukwu-eric <onukwubeizu@gmail.com>
2022-09-21 01:30:54 +0100

    new commit

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View } from 'react-native';
 import FeedScreen from './src/screens/FeedScreen';
-
+import CreatePostScreen from './src/screens/CreatePostScreen';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <FeedScreen />
+      {/* <FeedScreen /> */}
+      <CreatePostScreen />
       <StatusBar style="auto" />
     </View>
   );
--- src/screens/CreatePostScreen.js
+++ src/screens/CreatePostScreen.js
@@ -1,7 +1,63 @@
-import { Text } from "react-native"
+import { useState } from "react";
+import { Text, StyleSheet, View, Image, TextInput, Button } from "react-native"
 
+const user = {
+    id: "u1",
+    image:
+      "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/vadim.jpg",
+    name: "Vadim Savin",
+  };
+
+  
 const CreatePostScreen = () => {
-    return <Text>Create Post screen</Text>
+    const [ description, setDescription ] = useState("hello");
+
+    const onSubmit = () => {
+        console.warn("on submit", description)
+        setDescription("")
+    }
+    return (
+        <View style={styles.container}>
+            <View style={styles.header}>
+                <Image source={{ uri: user.image }} style={styles.image} />
+                <Text style={styles.name}>{user.name}</Text>
+            </View>
+
+            <TextInput value={description} onChangeText={setDescription} placeholder="what is on your mind?" multiline />
+            
+            <View style={styles.buttonContainer}>
+                <Button title="post" onPress={onSubmit} />
+            </View>
+        </View>
+    )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        width: "100%",
+        padding: 10,
+        paddingTop: 50,
+        backgroundColor: "#FFFFFF"
+    },
+    header: {
+        flexDirection: "row",
+        alignItems: "center",
+        width: "100%",
+        marginBottom: 10
+    },
+    image: {
+        height: 40,
+        width: 40,
+        borderRadius: 30,
+        marginRight: 10
+    },
+    name: {
+        fontWeight: "500"
+    },
+    buttonContainer: {
+        marginTop: "auto"
+    }
+})
 
 export default CreatePostScreen;
