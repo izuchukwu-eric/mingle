3c3018b563d293136c78b0d31b5a12b58c9609a6
izuchukwu-eric <onukwubeizu@gmail.com>
2022-09-21 04:27:15 +0100

    new commit

--- src/navigation/index.js
+++ src/navigation/index.js
@@ -1,8 +1,10 @@
 import FeedScreen from "../screens/FeedScreen"
 import CreatePostScreen from "../screens/CreatePostScreen";
 import ProfileScreen from "../screens/ProfileScreen";
+import UpdateProfileScreen from "../screens/UpdateProfileScreen";
 import { NavigationContainer } from "@react-navigation/native"
 import { createNativeStackNavigator } from "@react-navigation/native-stack"
+import { FontAwesome } from "@expo/vector-icons"
 
 
 const Stack = createNativeStackNavigator();
@@ -11,9 +13,20 @@ const Navigatior = () => {
     return (
         <NavigationContainer>
             <Stack.Navigator>
-                <Stack.Screen name="Feed" component={FeedScreen} />
+                <Stack.Screen name="Feed" component={FeedScreen} options={({ navigation }) => ({
+                        headerRight: () => (
+                        <FontAwesome
+                            onPress={() => navigation.navigate("Profile")}
+                            name="user"
+                            size={24}
+                            color="gray"
+                        />
+                        ),
+                    })}
+                />
                 <Stack.Screen name="Create Post" component={CreatePostScreen} />
                 <Stack.Screen name="Profile" component={ProfileScreen} />
+                <Stack.Screen name="Update Profile" component={UpdateProfileScreen} />
             </Stack.Navigator>
         </NavigationContainer>
     )
